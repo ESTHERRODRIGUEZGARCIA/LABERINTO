c71518cff02b38cd2e28074647cb7c317625e915
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-29 17:48:09 +0100

    cambios

--- calcular_módulo.py
+++ calcular_módulo.py
@@ -3,13 +3,20 @@
 from math import sqrt
 
 class Punto:
-    def __init__(self, a, b):
+    def __init__(self, a, b, c):
         self.a = a
         self.b = b
+        self.c = c
+
+def mostrar(self):
+    print("Punto ({}, {}, {})".format(self.a, self.b, self.c))
+
+
 def calculo_modulo(p1,p2):
     return sqrt((p1.a - p2.a)**2 + (p1.b - p2.b)**2)
 
 punto1 = Punto(3, 2)
-punto2 = Punto (-3, -5)
+punto2 = Punto(0, 0)
 
+print("La distancia del punto respecto al origen es de: ")
 print(calculo_modulo(punto1, punto2))
---
+++ distancia_puntos.py
@@ -0,0 +1,4 @@
+#Ejercicio: Cree un método que permita calcular la distancia de un punto en curso respecto a otro.
+from math import sqrt
+class punto:
+    def __init__
