da1f4fdb0ebe221f85c887206c60b5ba6d050ea5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-29 19:01:23 +0100

    3 ejercicios, fallos: siguiente commit

--- ejercicio1_los3.py
+++ ejercicio1_los3.py
@@ -16,9 +16,18 @@ def calculo_modulo(self): #el módulo es sólo de un nº
     return (self.a**2 + self.b**2 + self.c**2) ** (1 / 2)
 
 #2. Ejercicio: Cree un método que permita calcular la distancia de un punto en curso respecto a otro.
+def calculo_distancia(self, otropunto):
+    return((self.a-otropunto.a)**2 + (self.b-otropunto.b)**2 + (self.c-otropunto.c)**2 )**(1/2)
 
 #3. Ejercicio: Cree un método que permita calcular la distancia del punto en curso respecto al origen (es decir, el módulo).
+def distacia_punto_origen(self, origen):
+    origen == (0, 0, 0)
+    return((self.a-origen.a)**2 + (self.b-origen.b)**2 + (self.c-origen.c)**2 )**(1/2)
 
 #Para todos:
 p = Punto(13, 23, 8)
 p.mostrar()
+print("|p| =", p.calculo_modulo())
+print("distancia entre p y (1, 2, 5) es ", p.calculo_distancia(Punto(1, 2, 5)))
+print("|p| =", p.distancia_punto_origen())
+print("distancia entre p y (1, 2, 5) es ", p.distancia_punto_origen(Punto(1, 2, 5)))
