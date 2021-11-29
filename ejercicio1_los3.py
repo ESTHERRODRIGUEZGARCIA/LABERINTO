5e65fb4ca3b8d873415f743efcceae0c6c7ae2a9
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-29 18:47:13 +0100

    Fallo 1: No va "mostrar"

---
+++ ejercicio1_los3.py
@@ -0,0 +1,24 @@
+#Hago los 3 ejercicios aquí
+
+#1. Ejercicio: Cree un método que permita calcular el módulo del punto (distancia respecto al origen).
+from math import sqrt
+class Punto:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+def mostrar(self):
+    print("Punto ({}, {}, {})".format(self.a, self.b, self.c))
+
+
+def calculo_modulo(self): #el módulo es sólo de un nº
+    return (self.a**2 + self.b**2 + self.c**2) ** (1 / 2)
+
+#2. Ejercicio: Cree un método que permita calcular la distancia de un punto en curso respecto a otro.
+
+#3. Ejercicio: Cree un método que permita calcular la distancia del punto en curso respecto al origen (es decir, el módulo).
+
+#Para todos:
+p = Punto(13, 23, 8)
+p.mostrar()
