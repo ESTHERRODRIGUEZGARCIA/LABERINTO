20bee5c86e9981355d1e875f3ebb324009f8c6b5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-07 19:36:15 +0100

    tengo que saber como hacer este bucle

--- laberinto_tareafinal.py
+++ laberinto_tareafinal.py
@@ -1,8 +1,9 @@
 #tarea 2 LABERINTO
 #sol: ['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha', 'Abajo', 'Abajo', 'Abajo']
 def recorrido(tablero):
-    #Ahora el tablero es solo de una fila
+    #Consiste en realizar el laberinto, no hay tablero
     fila = columnas = 0
     movimientos = ['Abajo', 'Arriba', 'Derecha']
+    while 
 
 print("Solución: ", recorrido(tablero))    
