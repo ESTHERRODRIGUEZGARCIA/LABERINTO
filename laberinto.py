f9c98fc00721b7fbee9081911eff6f77a1c44c34
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-07 13:36:12 +0100

    TAREA 1 REALIZADA. ME SALEEE :)

--- laberinto.py
+++ laberinto.py
@@ -11,15 +11,18 @@ def tablero(laberinto, muro):
     ]
 
 #i van a ser las filas, que son 5. Ya definidas supongo
-#probando
-for i in range (laberinto):
-    fila = []
 
-return tablero    
+    for i in range (laberinto):
+        fila = []
+    return tablero
 
 
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 
+labyrinth = tablero(5, muro)
+
+for i in labyrinth:
+    print("".join(i))
 
 
 
