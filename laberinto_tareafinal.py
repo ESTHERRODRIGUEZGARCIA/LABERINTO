b23e603acafbc1dea79f59e242393a8b8b973ee5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-08 20:25:21 +0100

    sin comentarios

--- laberinto_tareafinal.py
+++ laberinto_tareafinal.py
@@ -2,11 +2,11 @@
 #sol: ['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha', 'Abajo', 'Abajo', 'Abajo']
 def tablero(laberinto, muro):
     tablero = [
-    [' E', ' X', ' X', ' X', ' X'], 
+    ['  ', ' X', ' X', ' X', ' X'], 
     ['  ', ' X', '  ', '  ', '  '],
     ['  ', ' X', '  ', ' X', '  '], 
     ['  ', '  ', '  ', ' X', '  '], 
-    [' X', ' X', ' X', ' X', ' S']
+    [' X', ' X', ' X', ' X', '  ']
     ]
 
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
@@ -14,25 +14,30 @@ muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4
 
 
 def recorrido(tablero):
-        #Consiste en realizar el laberinto, no hay tablero
+    #Consiste en realizar el laberinto, no hay tablero
     fila = columnas = 0
-    mov = [' ']
-    while (fila < n-1 and columnas < n-1):
-        if mov != 'Arriba' and fila + 1 < n and columnas != 'X':
+    mov = ['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha', 'Abajo', 'Abajo', 'Abajo']
+    
+    while (fila < 0 and columnas < 0):
+        if mov != 'Arriba' and fila + 1 < 0 and recorrido[fila + 1 ][columnas] != 'X':
             fila += 1
+            #la fila sería una más, la columna queda quieta en el recorrido
             print("Abajo")
-        elif mov != 'Abajo' and fila -1 > 0 and columnas != 'X':
+        elif mov != 'Abajo' and fila -1 > 0 and recorrido[fila - 1 ][columnas] != 'X':
             fila -= 1
+            #la fila sería una menos, la columna queda quieta en el recorrido
             print("Arriba")
-        elif mov != 'Izquierda' and fila -1 > 0 and columnas != 'X':
-            columnas -= 1
+        elif mov != 'Izquierda' and fila -1 > 0 and recorrido[fila][columnas + 1] != 'X':
+            columnas += 1
+            #al contrario, la fila queda quieta en el recorrido, la columna sería una más
             print("Derecha")
-        elif mov != 'Derecha' and fila -1 > 0 and columnas != 'X':
+        elif mov != 'Derecha' and fila -1 > 0 and recorrido[fila][columnas - 1] != 'X':
             columnas -= 1
+            #en este caso la columna es la anterior
             print("izquierda")
         else:
             print("no hay salida")
+    return mov
             
-
 
 print("Solución: ", recorrido(tablero))   
