7c26559f72b97fdb6deb6460fa7b7a4fad65af02
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-08 20:54:09 +0100

    Ese no era el fallo

--- laberinto_tareafinal.py
+++ laberinto_tareafinal.py
@@ -15,27 +15,27 @@ muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4
 def recorrido(tablero):
     #Consiste en realizar el laberinto, no hay tablero
     fila = columnas = 1 # son las iniciales
-    mov = ['A']
+    mov = ['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha', 'Abajo', 'Abajo', 'Abajo']
 
     while (fila < 1 and columnas < 1):
         if mov != 'Arriba' and fila + 1 < 1 and recorrido[fila + 1 ][columnas] != 'X':
             fila += 1
             #la fila sería una más, la columna queda quieta en el recorrido
-            mov.append("Abajo")
+            print("Abajo")
         elif mov != 'Abajo' and fila -1 > 0 and recorrido[fila - 1 ][columnas] != 'X':
             fila -= 1
             #la fila sería una menos, la columna queda quieta en el recorrido
-            mov.append("Arriba")
+            print("Arriba")
         elif mov != 'Izquierda' and fila -1 > 1 and recorrido[fila][columnas + 1] != 'X':
             columnas += 1
             #al contrario, la fila queda quieta en el recorrido, la columna sería una más
-            mov.append("Derecha")
+            print("Derecha")
         elif mov != 'Derecha' and fila -1 > 0 and recorrido[fila][columnas - 1] != 'X':
             columnas -= 1
             #en este caso la columna es la anterior
-            mov.append("izquierda")
+            print("izquierda")
         else:
-            mov.append("no hay salida")
+            print("no hay salida")
     return mov  
 
 print("Solución: ", recorrido(tablero))   
