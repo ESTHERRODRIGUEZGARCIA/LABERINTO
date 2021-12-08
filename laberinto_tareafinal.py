8dc89302950c1f0cc9e91466092cdd89cbde099e
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-08 20:53:00 +0100

    no es con print...

--- laberinto_tareafinal.py
+++ laberinto_tareafinal.py
@@ -1,5 +1,6 @@
 #tarea 2 LABERINTO
 #sol: ['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha', 'Abajo', 'Abajo', 'Abajo']
+
 def tablero(laberinto, muro):
     tablero = [
     ['  ', ' X', ' X', ' X', ' X'], 
@@ -11,33 +12,30 @@ def tablero(laberinto, muro):
 
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 
-
-
 def recorrido(tablero):
     #Consiste en realizar el laberinto, no hay tablero
-    fila = columnas = 0
-    mov = ['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha', 'Abajo', 'Abajo', 'Abajo']
-    
-    while (fila < 0 and columnas < 0):
-        if mov != 'Arriba' and fila + 1 < 0 and recorrido[fila + 1 ][columnas] != 'X':
+    fila = columnas = 1 # son las iniciales
+    mov = ['A']
+
+    while (fila < 1 and columnas < 1):
+        if mov != 'Arriba' and fila + 1 < 1 and recorrido[fila + 1 ][columnas] != 'X':
             fila += 1
             #la fila sería una más, la columna queda quieta en el recorrido
-            print("Abajo")
+            mov.append("Abajo")
         elif mov != 'Abajo' and fila -1 > 0 and recorrido[fila - 1 ][columnas] != 'X':
             fila -= 1
             #la fila sería una menos, la columna queda quieta en el recorrido
-            print("Arriba")
-        elif mov != 'Izquierda' and fila -1 > 0 and recorrido[fila][columnas + 1] != 'X':
+            mov.append("Arriba")
+        elif mov != 'Izquierda' and fila -1 > 1 and recorrido[fila][columnas + 1] != 'X':
             columnas += 1
             #al contrario, la fila queda quieta en el recorrido, la columna sería una más
-            print("Derecha")
+            mov.append("Derecha")
         elif mov != 'Derecha' and fila -1 > 0 and recorrido[fila][columnas - 1] != 'X':
             columnas -= 1
             #en este caso la columna es la anterior
-            print("izquierda")
+            mov.append("izquierda")
         else:
-            print("no hay salida")
-    return mov
-            
+            mov.append("no hay salida")
+    return mov  
 
 print("Solución: ", recorrido(tablero))   
